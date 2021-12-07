a0b63ae709ed0a55e54f811fdab7c6b3fbb5a1ef
yogizafitrah <yogi.zafitrah@gmail.com>
2021-12-07 09:18:58 +0700

    commit

---
+++ app/src/main/java/com/cloudteam/ecg/SplashScreen.java
@@ -0,0 +1,30 @@
+package com.cloudteam.ecg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Menghilangkan Acion Bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash_screen);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                finish();
+            }
+        },3000L); //3000 L = 3 detik
+    }
+}
